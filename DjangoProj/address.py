bcb25be9488f0aab1378cc70df114400e4217104
Hkakashi <43193113+Hkakashi@users.noreply.github.com>
2018-12-25 19:58:30 +0800

    Delete address.py

--- DjangoProj/address.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render_to_response
-
-address=[
-    {'name':'Jay','address':'nus'},
-    {'name':'Ray','address':'astar'}
-]
-
-def index(request):
-    return render_to_response('address.html',{'address':address})
